217eba5f3b27b109b2cb50dcadf1599207cb4562
tamnguyen2k <ntam37285@gmail.com>
2025-05-18 18:46:54 +0700

    Initial second commit

--- selenium/webdriver/Topic_01_Check_Environment.java
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -15,7 +15,7 @@ public class Topic_01_Check_Environment {
         driver.get("https://www.facebook.com/");
         driver.quit();
     }
-    //new
+    //newji
 
     @Test
     public void TC_02_Run_On_Chrome() {
